3ec07dc9b6a90de09f50ddfd23d0c32ceeefbc07
Andresa <curso@coopersystem.local>
2022-03-21 19:21:33 -0300

    H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resouces/CategoryResouce.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscatalog.resouces;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+
+//Recurso da entidade
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResouce {
+
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
