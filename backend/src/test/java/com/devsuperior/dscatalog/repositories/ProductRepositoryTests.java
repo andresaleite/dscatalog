106bd3751fc11893631657777403973c71d564f7
Andresa <curso@coopersystem.local>
2022-03-30 21:18:36 -0300

    First unit test

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dscatalog.entities.Product;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+	
+	@Autowired
+	private ProductRepository repository;
+
+	@Test
+	public void deleteSholdDeleteObjectWhenIdExist() {
+		
+		long id = 1L;
+		repository.deleteById(id);
+		Optional<Product> result = repository.findById(id);
+		Assertions.assertFalse(result.isPresent());
+		
+		
+	}
+
+	
+}
