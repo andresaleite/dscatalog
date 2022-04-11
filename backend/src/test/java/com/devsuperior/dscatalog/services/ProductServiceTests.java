0ea33e5dbe80371f521729a6742bbcd9675215c1
Andresa <curso@coopersystem.local>
2022-04-10 21:04:39 -0300

    02-27 Simulando comportamentos diversos com Mockito

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -4,20 +4,30 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -31,17 +41,30 @@ public class ProductServiceTests {
 	private long existingId;
 	private long nonExistingId;
 	private long dependentId;
+	private PageImpl<Product> page;
+	private Product product;
+	private ProductDTO productDto;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		 existingId = 1L;
-		 nonExistingId = 100L;
-		 dependentId = 4L;
+		 nonExistingId = 2L;
+		 dependentId = 3L;
+		 product = Factory.createProduct();
+		 productDto = Factory.createProductDTO();
+		 page = new PageImpl<>(List.of(product));
+		 
+		 when(repository.findById(existingId)).thenReturn(Optional.of(product));
+		 when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+		 when(repository.save(product)).thenReturn(product);
+		 when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
 		 
 		 doNothing().when(repository).deleteById(existingId);;
 		 doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);;
 		 doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);;
 	}
+	
+	
 	
 	@Test
 	public void deleteShouldThrowDatabaseExceptionWhenDependId() {
